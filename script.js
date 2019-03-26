a189dfb2fecf5b931f79b231fa97f1161581c202
DominikTschimmel <DominikTschimmel@users.noreply.github.com>
2019-03-26 02:07:02 +0100

    update

--- script.js
+++ script.js
@@ -8,15 +8,6 @@
   	}
 	}
 	
-	function HideMenu() {
-		var x = document.documentElement.clientWidth
-		var y = document.getElementById("navigation");
-		if (x <  1050) {
-    	y.style.display = "none";
-  		}  
-		else {
-		y.style.display = "block";
-		}
-		}
+
 
 

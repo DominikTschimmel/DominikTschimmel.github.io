9295dfcd7f5846fd8f90b655f1516234d53dbee5
DominikTschimmel <35820061+DominikTschimmel@users.noreply.github.com>
2019-07-27 03:33:45 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+	var img = document.querySelector("image");
 	document.body.classList.add('js-loading');
 
-	window.addEventListener("load", showPage);
+	img.addEventListener("load", showPage);
 
 	unction showPage() {
   	document.body.classList.remove('js-loading');	}

2ba2c6245a3325127c39e702c1026ef0636f7cf5
Dominik Tschimmel <dominik.tschimmel.contact@gmail.com>
2022-08-09 04:28:49 +0200

    update

---
+++ script.js
@@ -0,0 +1,28 @@
+// load images in the cache 
+
+
+ 	var img1 = new Image();
+    var img2 = new Image();
+    var img3 = new Image();
+    var img4 = new Image();
+    
+    img1.src = "images/About.jpg";
+    img2.src = "images/Contact.jpg";
+    img3.src = "images/Notes.jpg";
+    img4.src = "images/Other_Stuff.jpg";
+    
+    
+	
+	
+
+
+
+
+
+
+ 
+
+
+
+
+
